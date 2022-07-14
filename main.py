ea8f4fb35c4723f683323ca7d0f1962fd3aa79c0
Ethan Cho <ethan.cho1826@gmail.com>
2022-07-14 16:06:29 -0500

    prompts finished

---
+++ main.py
@@ -0,0 +1,7 @@
+print("welcome to the tip calculator")
+
+bill = float(input("What was the total bill? "))
+
+tip_percentage = float(input("What percentage tip would you like to give?"))
+
+people_split = int(input("How many people to split the bill? "))
